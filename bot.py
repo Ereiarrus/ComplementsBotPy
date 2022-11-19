a4325da5c293402cef230948f737e0a438ef81cf
Ereiarrus <me.he.jey+ereiarrus@gmail.com>
2022-11-19 01:08:35 +0000

    checking if new environment works

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os  # for importing env vars for the bot to use
 from twitchio.ext import commands
 
 bot = commands.Bot(
-    # set up the bot
+    # bot setuppip in
     token=os.environ['TMI_TOKEN'],
     client_id=os.environ['CLIENT_ID'],
     nick=os.environ['BOT_NICK'],
