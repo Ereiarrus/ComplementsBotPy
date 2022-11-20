8adaa5d53a3bef0442008099e966ef9d99e16dae
Ereiarrus <me.he.jey+ereiarrus@gmail.com>
2022-11-20 04:48:30 +0000

    moved some environment variables into code, and changed readme to reflect change; implemented ignoreme and unignoreme; added some boilerplate code for rest of commands

--- README.md
+++ README.md
@@ -1,21 +1,26 @@
 # complementBot_Twitch
 Make sure you have Python installed (Python 3.10.6 was used): https://www.python.org/downloads/
 
+Twitch channel: https://www.twitch.tv/complementsbot
+Also check out https://www.twitch.tv/ereiarrus (if I ever decide to stream...)
+
 Make sure create a .env file in the root directory with the following variables:
 - TMI_TOKEN= get your token from https://twitchapps.com/tmi/
-- CLIENT_ID= register your app with twitch on https://dev.twitch.tv/console/apps/create - for name, give it the channel name (ComplementsBot in our case), OAuth Redirect URLs: https:localhost:8000, Category: Chat Bot; then go to 'Manage' and copy the Client ID 
-- BOT_NICK= give it the channel name (ComplementsBot)
-- BOT_PREFIX= messages beginning with this shoul be listened out for by the bot; this is usually '!'.
-- CHANNEL= give it the name of the channel where you want the bot to be active
+- CLIENT_ID= register your app with twitch on https://dev.twitch.tv/console/apps/create - for name, give it the channel name (ComplementsBot in our case), OAuth Redirect URLs: https:localhost:8000, Category: Chat Bot; then go to 'Manage' and copy the Client ID
+- CHANNELS= give it the name of the channel where you want the bot to be active, separated by colons (':')
+- IGNORED_USERS= List of ignored users, seperated by colons
 
 
 
-I complement the sender of a message with a 5% chance. I currently have about 50 ways to complement you. 
+I complement the sender of a message with a 3.33% chance. I currently have about 50 ways to complement you. 
 Source code: https://github.com/Ereiarrus/ComplementsBotPy
 
 The following commands have been implemented:
 
 Say these commands in my channel chat here:
+- !count - Check out how many channels I'm in
+
+The following commands work anywhere that I have joined:
 - !complement \[username\] - If username present, complement that user; otherwise, get a complement yourself!
 
 The following commands have not been implemented yet, but are planned to be:
@@ -24,7 +29,6 @@ Say these commands in my channel chat here:
 - !joinme - I will join your channel
 - !leaveme - I will leave your channel
 - !about - Learn all about me
-- !count - Check out how many channels I'm in
 - !ignoreme - I will never complement you
 - !unignoreme - undo !ignoreme
 
@@ -32,4 +36,4 @@ These commands work in any channel I'm in, but must be used by the channel owner
 - !setchance - change how likely it is that person sending message gets complemented
 - !addcomplement <complement> - add a custom complement for to your own channel
 - !removecomplement <complement> - remove a complement from your own channel
-- !listcomplements - lists all complements, including the default and channel owner added ones
+- !listcomplements - lists all complements which have been added
--- bot.py
+++ bot.py
@@ -2,10 +2,19 @@
 import os  # for importing env vars for the bot to use
 from twitchio.ext import commands
 import random
+from threading import Lock
 
-CMD_PREFIX = os.environ['BOT_PREFIX']
-TTS_IGNORE_PREFIX = os.environ['TTS_IGNORE_PREFIX']
-COMPLEMENT_CHANCE = 5
+DEFAULT_CMD_PREFIX = '!'
+DEFAULT_TTS_IGNORE_PREFIX = "! "
+OWNER_NICK = 'ereiarrus'
+BOT_NICK = "complementsbot"
+DEFAULT_COMPLEMENT_CHANCE = 10.0/3.0
+
+channels_to_join_lock = Lock()
+CHANNELS_TO_JOIN = set(os.environ['CHANNELS'].split(':'))
+
+ignored_users_lock = Lock()
+IGNORED_USERS = set(os.environ['IGNORED_USERS'].split(':'))
 
 
 class Bot(commands.Bot):
@@ -13,9 +22,9 @@ class Bot(commands.Bot):
         super().__init__(
             token=os.environ['TMI_TOKEN'],
             client_id=os.environ['CLIENT_ID'],
-            nick=os.environ['BOT_NICK'],
-            prefix=CMD_PREFIX,
-            initial_channels=[os.environ['CHANNEL']]
+            nick=BOT_NICK,
+            prefix=DEFAULT_CMD_PREFIX,
+            initial_channels=list(CHANNELS_TO_JOIN)
         )
         self.COMPLEMENTS_LIST = []
         with open("complements_list.txt", "r") as f:
@@ -24,7 +33,7 @@ class Bot(commands.Bot):
 
     async def event_ready(self):
         # Called once when the bot goes online.
-        print(f"{os.environ['BOT_NICK']} is online!")
+        print(f"{BOT_NICK} is online!")
 
     async def event_message(self, ctx):
         # Runs every time a message is sent in chat.
@@ -32,9 +41,9 @@ class Bot(commands.Bot):
             # make sure the bot ignores itself
             return
 
-        if ctx.content[:len(CMD_PREFIX)] == CMD_PREFIX:
+        if ctx.content[:len(DEFAULT_CMD_PREFIX)] == DEFAULT_CMD_PREFIX:
             await self.handle_commands(ctx)
-        elif (random.random() * 100) <= COMPLEMENT_CHANCE:
+        elif (random.random() * 100) <= DEFAULT_COMPLEMENT_CHANCE and (not (ctx.author.name in IGNORED_USERS)):
             await ctx.channel.send(self.complement_msg(ctx, ctx.author.name))
 
     def choose_complement(self):
@@ -42,15 +51,10 @@ class Bot(commands.Bot):
 
     def complement_msg(self, ctx, who=None):
         split_msg = ctx.content.split()
-
         if not who:
             who = ctx.author.name
-
-        should_at = who[0] != "@"
-        atted_user = who
-        if should_at:
-            atted_user = "@" + who
-        return TTS_IGNORE_PREFIX + atted_user + " " + self.choose_complement()
+        who = "@" + who
+        return DEFAULT_TTS_IGNORE_PREFIX + who + " " + self.choose_complement()
 
     @commands.command()
     async def complement(self, ctx):
@@ -59,60 +63,115 @@ class Bot(commands.Bot):
         who = ctx.message.author.name
         if len(args) > 1:
             who = args[1]
+            if who[0] == "@":
+                who = who[1:]
+        if who in IGNORED_USERS:
+            return
         await ctx.channel.send(self.complement_msg(ctx.message, who))
 
     # -------------------- bot channel only commands --------------------
+
+    @staticmethod
+    def is_in_bot_channel(ctx):
+        return ctx.channel.name == BOT_NICK or ctx.channel.name == OWNER_NICK
+
     @commands.command()
     async def joinme(self, ctx):
         # I will join your channel!
-        pass
+        if not self.is_in_bot_channel(ctx):
+            return
+        try:
+            channels_to_join_lock.acquire()
+        except Exception as e:
+            raise e
+        finally:
+            channels_to_join_lock.release()
 
     @commands.command()
     async def leaveme(self, ctx):
         # I will leave your channel
-        pass
+        if not self.is_in_bot_channel(ctx):
+            return
+        try:
+            channels_to_join_lock.acquire()
+        except Exception as e:
+            raise e
+        finally:
+            channels_to_join_lock.release()
 
     @commands.command()
     async def about(self, ctx):
         # learn all about me
-        pass
+        if not self.is_in_bot_channel(ctx):
+            return
 
     @commands.command()
     async def count(self, ctx):
         # see how many channels I'm in
-        pass
+        if not self.is_in_bot_channel(ctx):
+            return
+        ctx.channel.send("@" + ctx.message.author.name + str(len(CHANNELS_TO_JOIN)) + " channels and counting!")
 
     @commands.command()
     async def ignoreme(self, ctx):
         # no longer complement the user
-        pass
+        if not self.is_in_bot_channel(ctx):
+            return
+        try:
+            ignored_users_lock.acquire()
+            user = ctx.author.name
+            IGNORED_USERS.add(user)
+            # TODO: the line below can probably be ran periodically instead of on every !ignoreme
+            os.environ["IGNORED_USERS"] = ':'.join(IGNORED_USERS)
+        except Exception as e:
+            raise e
+        finally:
+            ignored_users_lock.release()
 
     @commands.command()
     async def unignoreme(self, ctx):
         # undo ignoreme
-        pass
+        if not self.is_in_bot_channel(ctx):
+            return
+        try:
+            ignored_users_lock.acquire()
+            IGNORED_USERS.remove(ctx.author.name)
+            # TODO: the line below can probably be ran periodically instead of on every !unignoreme
+            os.environ["IGNORED_USERS"] = ':'.join(IGNORED_USERS)
+        except Exception as e:
+            raise e
+        finally:
+            ignored_users_lock.release()
 
     # -------------------- any channel, but must be by owner --------------------
+
+    @staticmethod
+    def is_by_channel_owner(ctx):
+        return ctx.channel.name == ctx.author.name
 
     @commands.command()
     async def setchance(self, ctx):
         # change how likely it is that person sending message gets complemented
-        pass
+        if not self.is_by_channel_owner(ctx):
+            return
 
     @commands.command()
     async def addcomplement(self, ctx):
         # add a custom complement for owner's channel
-        pass
+        if not self.is_by_channel_owner(ctx):
+            return
 
     @commands.command()
     async def removecomplement(self, ctx):
-        # remove a custom comple
-        pass
+        # remove a custom complement
+        if not self.is_by_channel_owner(ctx):
+            return
 
     @commands.command()
     async def listcomplements(self, ctx):
-        # see how many channels I'm in
-        pass
+        # list all extra complements
+        if not self.is_by_channel_owner(ctx):
+            return
 
 
 if __name__ == "__main__":
