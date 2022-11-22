195a755b91f27e456b47e0270f1feff736d090af
Ereiarrus <112237359+Ereiarrus@users.noreply.github.com>
2022-11-22 06:36:22 +0000

    using .child() instead of steing concatenation; instead of push, set needed to be used to add DEFAULT_USER

--- database.py
+++ database.py
@@ -6,12 +6,10 @@ cred = credentials.Certificate("./.firebase_config.json")
 # firebase_admin.initialize_app(cred)
 databaseURL = os.environ['DATABASE_URL']
 firebase_admin.initialize_app(cred, {'databaseURL': databaseURL})
-PATH_SEPERATOR = "/"
 
-IGNORED_PATH_DB = PATH_SEPERATOR + "Ignored"
-IGNORED_DB_REF = db.reference(IGNORED_PATH_DB)
-USERS_PATH_DB = PATH_SEPERATOR + "Users"
-USERS_DB_REF = db.reference(USERS_PATH_DB)
+REF = db.reference('/')
+IGNORED_DB_REF = ref.child('Ignored')
+USERS_DB_REF = ref.child('Users')
 
 DEFAULT_COMPLEMENT_CHANCE = 10.0 / 3.0
 DEFAULT_SHOULD_IGNORE_BOTS = True
@@ -36,7 +34,7 @@ def ignore(user):
 
 
 def unignore(user):
-    db.reference(IGNORED_PATH_DB + PATH_SEPERATOR + user).delete()
+    IGNORED_DB_REF.child(user).delete()
 
 
 def channel_exists(user):
@@ -47,31 +45,22 @@ def is_channel_joined(user):
     if not channel_exists(user):
         return False
 
-    user_is_joined_path = USERS_PATH_DB + PATH_SEPERATOR + user + PATH_SEPERATOR + IS_JOINED
-    user_is_joined_ref = db.reference(user_is_joined_path)
-    return user_is_joined_ref.get()
+    return USERS_DB_REF.child(user).child(IS_JOINED).get()
 
 
 def join_channel(user):
     if not channel_exists:
-        USERS_DB_REF.push({user: DEFAULT_USER})
-        return
-
-    user_is_joined_path = USERS_PATH_DB + PATH_SEPERATOR + user + PATH_SEPERATOR + IS_JOINED
-    user_is_joined_ref = db.reference(user_is_joined_path)
-    user_is_joined_ref.set(True)
+        USERS_DB_REF.set({user: DEFAULT_USER})
+    else:
+        USERS_DB_REF.child(user).child(IS_JOINED).set(True)
 
 
 def leave_channel(user):
-    user_is_joined_path = USERS_PATH_DB + PATH_SEPERATOR + user + PATH_SEPERATOR + IS_JOINED
-    user_is_joined_ref = db.reference(user_is_joined_path)
-    user_is_joined_ref.set(False)
+    USERS_DB_REF.child(user).child(IS_JOINED).set(False)
 
 
 def delete_channel(user):
-    user_path = USERS_PATH_DB + PATH_SEPERATOR + user
-    user_ref = db.reference(user_path)
-    user_ref.delete()
+    USERS_DB_REF.child(user).delete()
 
 
 def get_joined_channels():
@@ -79,9 +68,7 @@ def get_joined_channels():
     all_users = USERS_DB_REF.get(False, True)
     joined_users = []
     for user in all_users:
-        user_is_joined_path = USERS_PATH_DB + PATH_SEPERATOR + user + PATH_SEPERATOR + IS_JOINED
-        user_is_joined_ref = db.reference(user_is_joined_path)
-        if user_is_joined_ref.get():
+        if USERS_DB_REF.child(user).child(IS_JOINED).get():
             joined_users.append(user)
     return joined_users
 
@@ -96,11 +83,7 @@ def get_tts_ignore_prefix(user):
 
 
 def get_chance(user):
-    user_chance_path = USERS_PATH_DB + PATH_SEPERATOR + user + PATH_SEPERATOR + COMPLEMENT_CHANCE
-    user_chance_ref = db.reference(user_chance_path)
-    print(user)
-    print(user_chance_ref.get())
-    return user_chance_ref.get()
+    return USERS_DB_REF.child(user).child(COMPLEMENT_CHANCE).get()
 
 
 def ignore_bots(user):
