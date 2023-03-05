f09bbbc242b3dad565a92d2709f851774faf2031
Ereiarrus <me.he.jey+ereiarrus@gmail.com>
2023-03-05 20:07:02 +0000

    database is adapted to use user IDs now

--- ComplementsBot/bot.py
+++ ComplementsBot/bot.py
@@ -50,7 +50,7 @@ class ComplementsBot(commands.Bot):
     F_USER: str = "{user}"
     SHOULD_LOG: bool = True
     OWNER_NICK: str = 'ereiarrus'
-    OWNER_ID: int = 118034879
+    OWNER_ID: str = "118034879"
 
     def __init__(self) -> None:
         super().__init__(
@@ -83,11 +83,11 @@ class ComplementsBot(commands.Bot):
         Called once when the bot goes online; purely informational
         """
 
-        # joined_channels = await database.get_joined_channels()
-        joined_channels = [ComplementsBot.OWNER_ID, 845759020]
+        joined_channels = await database.get_joined_channels()
         max_num_user_reqs = 100
         for i in range((len(joined_channels) // max_num_user_reqs) + 1):
-            chunk = joined_channels[i * max_num_user_reqs: min((i + 1) * max_num_user_reqs, len(joined_channels))]
+            chunk = list(map(lambda x: int(x), joined_channels[i * max_num_user_reqs: min((i + 1) * max_num_user_reqs,
+                                                                                          len(joined_channels))]))
             channel_names = list(map(lambda x: x.user.name, await self.fetch_channels(broadcaster_ids=chunk)))
             await self.join_channels(channel_names)
         await database.join_channel(username=self.nick, name_to_id=self.name_to_id)
