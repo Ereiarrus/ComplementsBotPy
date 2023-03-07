bbe63fca2b674cbd7f87a7ea1846609d2aee8aa9
unknown <me.he.jey+ereiarrus@gmail.com>
2023-03-07 21:30:32 +0000

    completed script to start up docker container on VPS

--- run_in_docker.sh
+++ run_in_docker.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]; then
+  echo "Error: No token provided"
+  exit 1
+fi
+
 set -a
 source .env
 
-received_github_token=""
+received_github_token="$1"
 
 if "$TMI_TOKEN" != "$received_github_token"; then
   echo "Wrong sender; exiting"
   exit 1
 fi
 
+(exec "./docker_build.sh")
+if ! docker kill complements-bot-py >/dev/null 2>&1; then
+  echo "Did not find any pre-existing complements-bot-py process that could be terminated; continuing as normal.";
+fi
+# alternative:
+# if ! kill $(ps -fade | grep complements-bot-py | grep -v grep | awk '{print $2}') > /dev/null 2>&1; then echo "Did not find any pre-existing complements-bot-py process that could be terminated; continuing as normal." >&2; fi
 (exec "./docker_run.sh")
 
 set +a
