43fff5ceee8011540c95b292cf5541f96ef4aca3
unknown <me.he.jey+ereiarrus@gmail.com>
2023-03-29 21:04:34 +0100

    apt-get instead of apt

--- install_docker.sh
+++ install_docker.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
+sudo apt-get update
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt-get install docker-ce
