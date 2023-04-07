f865852b9842abc512f673e8f1ba474a68600431
unknown <me.he.jey+ereiarrus@gmail.com>
2023-04-07 02:13:57 +0100

    trying different scripts

--- docker_build.sh
+++ docker_build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build -t complements-bot-py .
+OLD_CONT="$(docker ps -q --filter ancestor=complements-bot-py)"
+docker build -t complements-bot-py . 1>/dev/null 2>&1
+
+echo "$OLD_CONT"
--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-docker stop "$(docker ps -q --filter ancestor=complements-bot-py)" || true
+if [ -z "$1" ]
+  then
+  docker stop "$(docker ps -q --filter ancestor=complements-bot-py)" || true
+else
+  docker stop "$1" || true
+fi
+
 docker run -dP complements-bot-py
