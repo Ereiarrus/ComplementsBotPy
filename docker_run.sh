67a875cc82db08b1e4917da25ffc25b5bd4adc93
unknown <me.he.jey+ereiarrus@gmail.com>
2023-06-11 21:46:19 +0100

    adding more logging

--- docker_run.sh
+++ docker_run.sh
@@ -29,11 +29,11 @@ container_id_file=./container_id.txt
 
 date +%s > "$STATUS_FILE"
 
-threshold=$((60 * 60 * 2))
+threshold=$((60 * 60))
 while [ "$(cat $container_id_file)" == "$container_id" ]; do
     if [ $(($(date +%s) - $(cat "$STATUS_FILE"))) -gt $threshold ]; then
-        (echo "Detected situation where docker container was working, but bot was not!") >> ./while_loop_log.txt
+        (echo "$container_id at $(date +%s) -  Detected situation where docker container was working, but bot was not!") >> ./while_loop_log.txt
         docker restart "$container_id"
     fi
-    sleep $((29 * 60))
+    sleep $((14 * 60))
 done >/dev/null 2>&1 &
