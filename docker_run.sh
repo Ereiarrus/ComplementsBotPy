f1ab77a33ec6fa3b3775d85b9e3916e20516069d
unknown <me.he.jey+ereiarrus@gmail.com>
2023-06-11 21:31:58 +0100

    redirecting output to dev null when stopping dockler container

--- docker_run.sh
+++ docker_run.sh
@@ -1,27 +1,30 @@
 #!/bin/bash
 
+
 STATUS_FILE=$1
 if [ -z "$STATUS_FILE"  ]; then
   STATUS_FILE=./status.txt
 fi
 
+
 old_container_id="$(./docker_build.sh)"
 build_status="$?"
 
-if [ "$build_status" != 0 ]
-  then
+
+if [ "$build_status" != 0 ]; then
   exit "$build_status"
 fi
 
 if [ -z "$old_container_id" ]; then
   docker stop "$(docker ps -q --filter ancestor=complements-bot-py)" || true
 else
-  docker stop "$old_container_id"
+  docker stop "$old_container_id" > /dev/null 2>&1
 fi
 
 container_id="$(docker run -dP --restart=unless-stopped -v ./status.txt:/status.txt --log-opt max-size=50m --log-opt max-file=3 complements-bot-py)"
 
 container_id_file=./container_id.txt
+
 (echo "$container_id") > "$container_id_file"
 
 date +%s > "$STATUS_FILE"
